3c6980737c12b9d4fe10fb50ec3b1351234f6d7e
Work Prahl <prahl@bme.ogi.edu>
2018-01-16 18:57:07 -0800

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 setup(
 	name='pygrin',
 	packages=['pygrin'],
-	version='1.0.2',
+	version='0.1.0',
 	description='Gradient Index (GRIN) Lens Calculations',
 	url='https://github.com/scottprahl/pygrin.git',  
 	author='Scott Prahl',
