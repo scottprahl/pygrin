3e8cee6a90db2bd2d2bcca2cd9f670a340731578
Scott Prahl <scott@prahls.com>
2020-01-21 20:35:51 -0800

    use README.md as long_description, update classifiers

--- setup.py
+++ setup.py
@@ -1,35 +1,54 @@
 """
-Copyright 2018 Scott Prahl
+Copyright 2018,2020 Scott Prahl
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 from setuptools import setup
 
+# use README as the long description
+# make sure to use the syntax that works in both ReST and markdown
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
 	name='pygrin',
 	packages=['pygrin'],
-	version='0.2.0',
+	version='0.3.0',
 	description='Gradient Index (GRIN) Lens Calculations',
 	url='https://github.com/scottprahl/pygrin.git',  
 	author='Scott Prahl',
 	author_email='scott.prahl@oit.edu',
 	license='MIT',
 	classifiers=[
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 		'License :: OSI Approved :: MIT License',
 		'Intended Audience :: Science/Research',
-		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.7',
 		'Topic :: Scientific/Engineering :: Physics',
 	],
 	keywords=['endoscope', 'pitch', 'SELFOC'],
 	install_requires=['numpy','matplotlib'],
-	long_description=
-	"""
-	A collection of routines for modeling and ray tracing through GRIN lenses.
-	""",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
